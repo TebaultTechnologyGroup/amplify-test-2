2407840024ab5b88348a0ccb9cbb4fe50e7cd712
TebaultTechnologyGroup <mark.tebault@tebaulttechnologygroup.com>
2025-12-12 21:29:04 -0500

    Removed todo data schema

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,38 +1,19 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 import { schema as generatedSqlSchema } from './schema.sql';
 
-
-// Add a global authorization rule
-// TODO: Update the authorization rule as needed for your application
-//.      https://docs.amplify.aws/react/build-a-backend/data/customize-authz/
-//const sqlSchema = generatedSqlSchema.authorization(allow => allow.guest())
+// Add authorization to the SQL schema
 const sqlSchema = generatedSqlSchema.authorization(allow => [
-  allow.authenticated(),
-  allow.owner()
+  allow.authenticated()
 ])
 
-// default todo schema from example app
-const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-       }).authorization(allow => [allow.owner()]);
-
-// combine with the generated SQL schema
-const combinedSchema = a.combine([schema, sqlSchema]);
-
-export type Schema = ClientSchema<typeof combinedSchema>;
-
+export type Schema = ClientSchema<typeof sqlSchema>;
 
 export const data = defineData({
-  schema,
+  schema: sqlSchema,
   authorizationModes: {
     defaultAuthorizationMode: 'userPool',
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
     },
-    
   },
 });
-
