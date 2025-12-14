abca15d1d461664489724b9336d1bef4a3ac833f
TebaultTechnologyGroup <mark.tebault@tebaulttechnologygroup.com>
2025-12-13 20:20:24 -0500

    added to authenticate

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,9 +1,13 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 import { schema as generatedSqlSchema } from './schema.sql';
+import { defineAuth } from '@aws-amplify/backend';
+
+
+// ////////////////// daabase ///////////////////////
 
 // Add authorization to the SQL schema
 const sqlSchema = generatedSqlSchema.authorization(allow => [
-  allow.authenticated()
+  allow.authenticated().to(['create', 'read', 'update'])
 ])
 
 export type Schema = ClientSchema<typeof sqlSchema>;
@@ -14,6 +18,28 @@ export const data = defineData({
     defaultAuthorizationMode: 'userPool',
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
+    },
+  },
+});
+
+// ////////////////// auth ///////////////////////
+
+/**
+ * Define and configure your auth resource
+ * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ */
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
+  },
+  userAttributes: {
+    givenName: {
+      mutable: true,
+      required: true,
+    },
+    familyName: {
+      mutable: true,
+      required: true,
     },
   },
 });
